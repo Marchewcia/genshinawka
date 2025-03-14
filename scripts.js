2e85a336f45974ce025d4a9727f8ced501fab710
Marchewcia <michalgolasinski6@gmail.com>
2025-03-14 20:52:14 +0100

    basic structure + first boss in weekly boss table

---
+++ scripts.js
@@ -0,0 +1,22 @@
+function enable(buttonNumber){
+    let index = buttonNumber - 1;
+    innerButton = document.getElementsByTagName("button")[index].innerHTML;
+    innerButton = innerButton.slice(4);
+    document.getElementsByTagName("button")[index].innerHTML = "Hide" + innerButton;
+    document.getElementsByTagName("button")[index].setAttribute("onclick", "disable(" + buttonNumber + ")")
+    document.getElementsByTagName("div")[index].style.display = "flex";
+}
+function disable(buttonNumber){
+    let index = buttonNumber - 1;
+    innerButton = document.getElementsByTagName("button")[index].innerHTML;
+    innerButton = innerButton.slice(4);
+    document.getElementsByTagName("button")[index].innerHTML = "Show" + innerButton;
+    document.getElementsByTagName("button")[index].setAttribute("onclick", "enable(" + buttonNumber + ")")
+    document.getElementsByTagName("div")[index].style.display = "none";
+}
+
+function getWeeklyTable(){
+    let website = document.getElementsByTagName("iframe")[0].innerHTML;
+    let tab = website.getElementsByTagName("table")[1];
+    document.getElementsByTagName("iframe")[0].innerHTML = tab;
+}
